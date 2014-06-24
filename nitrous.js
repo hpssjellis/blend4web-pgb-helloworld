4a778d9c1c4892182ab761d1ac41198121a83916
Jeremy Ellis <jellis@hpssapps.com>
2014-06-24 19:04:59 -0400

    Update nitrous.js

--- nitrous.js
+++ nitrous.js
@@ -11,6 +11,8 @@ var connect = require('connect'),
  */
 
 app.use(phonegap());
-
+console.log('Ctrl-C to quit');
+console.log('Click Preview in Nitrous.io to see the URL');
+console.log('Enter that URL into the Phonegap Developer App');
 var port = process.env.PORT || 3000;
 app.listen(port);
